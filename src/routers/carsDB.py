9b66004c779b9233deada2837bbe5953f84b031c
byandrev <byandrev@gmail.com>
2023-03-28 13:45:09 -0500

    config env files and create post car

--- src/routers/carsDB.py
+++ src/routers/carsDB.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI, APIRouter,HTTPException, status, Depends
 from db.models.schemas import Car
 from db.schemas.cars import car_schema
 from db.client import dbClient
+from bson.objectid import ObjectId
 
-app = FastAPI()
+# app = FastAPI()
 
 #class Car(car: Car):
  #   time: str
 
-router = APIRouter(prefix="/carsdb", tags="carsdb", responses=({status.HTTP_404_NOT_FOUND: {"message": "carro no encontrado"}}))
+router = APIRouter(prefix="/carsdb", tags=["carsdb"], responses=({status.HTTP_404_NOT_FOUND: {"message": "carro no encontrado"}}))
 
 
 carList = []
@@ -21,31 +22,29 @@ def searchCar(licensePlate: str):
     if not carFound:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="No se ha encontrado el vehiculo")
 
-@app.get("/")
+@router.get("/")
 async def cars():
     return carList
 
-@app.get("/{licensePlate}")
+@router.get("/{licensePlate}")
 async def getCar(licensePlate: str):
     return searchCar(licensePlate)
 
-@app.post("/")
-
+@router.post("/")
 async def postCar(car: Car):
  #   if type(searchCar(car.licensePlate)) == Car:
   #      raise HTTPException(status_code=status.HTTP_302_FOUND, detail="Carro ya existe")
 
     #carList.append(car)
-    carDict = dict (car)
-    del carDict["_id"]
-    id = dbClient.cars.insert_one(car).inserted_id
+    carDict = car.dict()
+    # del carDict["_id"]
 
-    newCar = car_schema(dbClient.cars.find_one({"_id": id}))
+    id = dbClient.cars.insert_one(carDict).inserted_id
+    #newCar = dbClient.cars.find_one({"_id": ObjectId(id)})
+    #return Car(**newCar)
+    return "YES"
 
-
-    return Car(**newCar)
-
-@app.put("/")
+@router.put("/")
 async def postCar(car: Car):
     for i in range(len(carList)):
         if car.licensePlate == carList[i].licensePlate:
@@ -53,7 +52,7 @@ async def postCar(car: Car):
             return car
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontró el vehículo")
 
-@app.delete("/{licensePlate}")
+@router.delete("/{licensePlate}")
 async def postCar(car: Car):
     if type(searchCar(car.licensePlate)) == Car:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Carro no se encuentra")
